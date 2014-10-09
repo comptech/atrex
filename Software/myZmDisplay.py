f9f676df1e93d3042bc555e891ad65d3050ca48f
hgarbeil <harold@higp.hawaii.edu>
2014-10-08 17:27:22 -1000

    Added zmDisplay.py to repository, required, of course, for zoom display.

---
+++ Software/myZmDisplay.py
@@ -0,0 +1,154 @@
+from PyQt4 import QtCore, QtGui
+
+import numpy as np
+from math import *
+from scipy.misc import imresize
+from scipy.ndimage import zoom
+
+class myZmDisplay (QtGui.QWidget) :
+    loadImage = 0
+    dispMax = 65535
+    dispMin = 0
+    zmFac = 4
+    newx = 0
+    newy = 0
+    
+    def __init__(self, parent) :
+        QtGui.QWidget.__init__(self, parent)
+
+    def setZmFac (self,zm) :
+        self.zmFac = zm
+
+
+    def setMinMax (self, min, max) :
+        self.dispMin = min
+        self.dispMax = max
+        print "(min max ) are :", min, max
+
+
+    # this is for the creating of the 32BPP QImage, currently using the lut version
+    def writeQImage (self, fulldata) :
+        self.xsize = self.width()
+        self.ysize = self.height()
+        self.fulldata = fulldata
+        tempdata = self.fulldata
+
+        # DN scaling
+        self.max = np.max (tempdata)/10.
+        self.min = np.min (tempdata)
+        range = self.max - self.min
+        self.scale = 255. / range
+
+        # size scaling
+        h,w = self.fulldata.shape
+        xscale = int(w/self.xsize)
+        yscale = int (h/self.ysize)
+
+        self.pscale = xscale
+        if (yscale > self.pscale) :
+            self.pscale = yscale
+
+        print 'scale is :', self.pscale
+        uarr = (self.scale * (self.fulldata - self.min)).astype(np.uint8)
+        uarr = uarr[::self.pscale, ::self.pscale]
+
+        a = np.zeros ((uarr.shape[0], uarr.shape[1],4), dtype=np.uint8)
+        a[:,:,3]=255
+        a[:,:,2]=255-uarr[:,:]
+        a[:,:,1]=255-uarr[:,:]
+        a[:,:,0]=255-uarr[:,:]
+
+        self.qimage = QtGui.QImage (a.data, a.shape[1], a.shape[0],QtGui.QImage.Format_ARGB32)
+        self.qimage.ndarray = a
+        self.loadImage = 1
+        self.repaint()
+
+    def setFulldata (self, fd) :
+        self.fulldata = fd 
+
+    
+    """ writeQImage_lut will scale the input raw data from 0 to 255 based upon
+        dispMin and dispMax values. Currently using a grey scale lut but will
+        build in several other options for color mapping DN.
+        centloc[0] is x coord of center, centloc[1] is y coord
+    """
+    def writeQImage_lut (self, fulldata, centloc) :
+        # for input square image,  simply resize image to smallest dimension
+        im_w = self.width()
+        im_h = self.height()
+        # based on the size of the window, determine number of pixels
+        # to extract from fulldata
+        numsamp = im_w / self.zmFac 
+        numline = im_h / self.zmFac 
+        ns2 = numsamp / 2
+        nl2 = numline / 2
+
+        newdim = im_w
+        if (im_h < im_w) :
+            newdim = im_h
+        
+        self.fulldata = fulldata
+        h,w = self.fulldata.shape
+        startx = centloc[0] - ns2
+        starty = centloc[1] - nl2
+        if (startx < 0) :
+            startx = 0
+        if (starty < 0) :
+            starty = 0
+        endx = startx + numsamp
+        endy = starty + numline
+        if (endx > w) :
+            endx = w
+        if (endy > h) :
+            endy = h 
+            
+        tempdata = self.fulldata [starty:endy,startx:endx] 
+
+
+        range255 = self.dispMax - self.dispMin
+        self.scale = 255. / range255
+        
+        #print '(im_w im_h disp_width disp_height)', w, h, self.xsize, self.ysize
+        #print 'scale is :', self.scalefac
+        uarr = (self.scale * (tempdata - self.dispMin)).astype(np.float)
+        uarr [uarr>255.] = 255.
+        uarr [uarr<0.] = 0.
+        uarr = uarr.astype(np.uint8)
+        #newarr = imresize (uarr, (newdim,newdim))
+
+        # zoom up the array by the zmFac
+        newarr = zoom (uarr, self.zmFac, order=3)
+        ysize = newarr.shape[1] / 4 * 4
+        xsize = newarr.shape[0] / 4 * 4
+
+        a = np.zeros ((xsize, ysize), dtype=np.uint8)
+        a[:,:]=255-newarr[0:ysize,0:xsize]
+        self.newx = a.shape[0]
+        self.newy = a.shape[1]
+        self.qimage = QtGui.QImage (a.data, a.shape[1], a.shape[0],
+                                    QtGui.QImage.Format_Indexed8)
+        #a[:,:,1]=255-uarr[:,:]
+        #a[:,:,0]=255-uarr[:,:]
+
+        # generate the lut
+        
+        for index in range(256) :
+            self.qimage.setColor (index, QtGui.qRgb (index, index, index))
+            
+        self.qimage.ndarray = a
+        self.loadImage = 1
+        self.repaint()
+
+    def paintEvent (self, event) :
+        w = self.width()
+        h = self.height()
+        dim = w
+        if (dim >h):
+            dim = h
+        
+        painter = QtGui.QPainter (self)
+        if (self.loadImage ==1) :
+                #painter.drawImage (0, 0, self.qimage, 0., 0., self.newx, self.newy)
+                painter.drawImage (0,0, self.qimage, 0.,0.)
+                
+                                
