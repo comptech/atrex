af388b6d4cc2cc6e935f0f06fa5576511c813ad3
hgarbeil <harold@higp.hawaii.edu>
2015-02-18 05:08:40 -1000

    Downloaded tifffile.c, created setup.py to create shared library. This gets rid of the ~gohlke warning message.

---
+++ Software/setup.py
@@ -0,0 +1,15 @@
+#setup.py
+
+"""A Python script to build the _tifffile extension module.
+
+Usage:: ``python setup.py build_ext --inplace``
+
+"""
+
+from distutils.core import setup, Extension
+import numpy
+
+setup(name='_tifffile', ext_modules=[Extension('_tifffile', ['tifffile.c'],
+    include_dirs=[numpy.get_include()], extra_compile_args=[])],)
+
+
